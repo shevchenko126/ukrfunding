a61995ca67be84e119c3f037e0ea08cbef513aae
Maximka47 <user2017max@gmail.com>
2022-06-26 23:47:06 +0300

    FundraiseCategory,  in Fundraise Foreignkey category(FundraiseCategory))
    Створена модель FundraiseCategory, і з Fundraise до неї ForeignKey з назвою category

--- backend/fundraises/admin.py
+++ backend/fundraises/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from fundraises.models import Fundraise
+from fundraises.models import Fundraise, FundraiseCategory
 
 
 admin.site.register(Fundraise)
+admin.site.register(FundraiseCategory)
 
