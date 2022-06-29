276667164e636e6e31551d8ce36556c9bf18bb74
4ugeistr <andrii.dutko@gmail.com>
2022-06-29 16:10:14 +0300

    added ForeignKey user to Fundraise class added import of User model to payment model added a test object to DB

--- backend/fundraises/models.py
+++ backend/fundraises/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class FundraiseCategory(models.Model):
     def save(self, *args, **kwargs):
         return super().save()
-    
+
     class Meta:
         verbose_name="Fundraise Category"
         verbose_name_plural="Fundraise Categories"
@@ -19,6 +20,12 @@ class Fundraise(models.Model):
                                                     on_delete = models.SET_NULL,
                                                     null = True,
                                                     blank = True)
+    user = models.ForeignKey(User,
+                             verbose_name='User',
+                             related_name='fundraise',
+                             on_delete=models.SET_NULL,
+                             null=True,
+                             blank=True)
 
     image = models.FileField(verbose_name="Зображення", upload_to = None, blank = True, null = True)
 
@@ -26,10 +33,10 @@ class Fundraise(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         return super().save()
-    
+
     class Meta:
         verbose_name="Fundraise"
         verbose_name_plural="Fundraises"
