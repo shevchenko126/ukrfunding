ca6191043695ff1d4fab13c2de279335683c42e6
DanyloDiachenko <107757498+DanyloDiachenko@users.noreply.github.com>
2022-07-18 13:05:59 +0300

    fix

--- frontend/src/components/contentBlocks/page4_filter_category/index.js
+++ frontend/src/components/contentBlocks/page4_filter_category/index.js
@@ -2,37 +2,37 @@
 
 const FilterCategory = () => {
 	return (
-		<div class="container filter p-0">
-			<div class="filter__title">Filter</div>
-			<div class="filter__categories">
-				<div class="filter__categories__title">Category</div>
-				<div class="d-flex filter__categories__str">
-					<input type="checkbox" class="filter__categories__checkbox" />
-					<div class="filter__categories__categories">All Projects</div>
+		<div className="container filter p-0">
+			<div className="filter__title">Filter</div>
+			<div className="filter__categories">
+				<div className="filter__categories__title">Category</div>
+				<div className="d-flex filter__categories__str">
+					<input type="checkbox" className="filter__categories__checkbox" />
+					<div className="filter__categories__categories">All Projects</div>
 				</div>
 				<hr />
-				<div class="d-flex filter__categories__str">
-					<input type="checkbox" class="filter__categories__checkbox" />
-					<div class="filter__categories__categories">Technology</div>
+				<div className="d-flex filter__categories__str">
+					<input type="checkbox" className="filter__categories__checkbox" />
+					<div className="filter__categories__categories">Technology</div>
 				</div>
 				<hr />
-				<div class="d-flex filter__categories__str">
-					<input type="checkbox" class="filter__categories__checkbox" />
-					<div class="filter__categories__categories">Medical</div>
+				<div className="d-flex filter__categories__str">
+					<input type="checkbox" className="filter__categories__checkbox" />
+					<div className="filter__categories__categories">Medical</div>
 				</div>
 				<hr />
-				<div class="d-flex filter__categories__str">
-					<input type="checkbox" class="filter__categories__checkbox" />
-					<div class="filter__categories__categories">Business</div>
+				<div className="d-flex filter__categories__str">
+					<input type="checkbox" className="filter__categories__checkbox" />
+					<div className="filter__categories__categories">Business</div>
 				</div>
 				<hr />
-				<div class="d-flex filter__categories__str">
-					<input type="checkbox" class="filter__categories__checkbox" />
-					<div class="filter__categories__categories">Fashion</div>
+				<div className="d-flex filter__categories__str">
+					<input type="checkbox" className="filter__categories__checkbox" />
+					<div className="filter__categories__categories">Fashion</div>
 				</div>
 				<hr />
-				<label class="filter__categories__see-more">See More Categories</label>
-				<select class="filter__categories__select">
+				<label className="filter__categories__see-more">See More Categories</label>
+				<select className="filter__categories__select">
 					<option></option>
 				</select>
 				<hr />
