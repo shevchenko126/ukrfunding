b5a9eb653c66e65aa514d032e6ad743fb2ba185e
Oksilama <6444144@gmail.com>
2022-07-19 16:52:55 +0300

    Update FormAddProject

--- frontend/src/components/contentBlocks/form_add_project/index.js
+++ frontend/src/components/contentBlocks/form_add_project/index.js
@@ -2,26 +2,26 @@ import './index.css';
 
 const FormAddProject = () => {
     return (
-        <div class="container add_project">
+        <div className="container__form_add_project">
             <h1> Your project </h1>
-            <div class="form">
+            <div className="form">
                 <form method="get" action="#">
-                    <div class="row mb-4">
+                    <div className="row mb-4">
                         <div class="col-lg-4 col-sm-12 mt-3">
                             <input type="text" placeholder="Name of project" />
                         </div>
-                        <div class="col-lg-4 col-sm-12 mt-3">
+                        <div className="col-lg-4 col-sm-12 mt-3">
                             <input class="input_image" type="file" placeholder="Image" />
                         </div>
-                        <div class="col-lg-4 col-sm-12 mt-3">
+                        <div className="col-lg-4 col-sm-12 mt-3">
                             <input type="number" placeholder="How much funding?" />
                         </div>
                     </div>
                     <textarea name="textarea" placeholder="Description"></textarea>
                 </form>
             </div>
-            <div class="button_add">
-                <button class="button">Add project</button></div>
+            <div className="button_add">
+                <button className="button">Add project</button></div>
         </div>
     )
 }
