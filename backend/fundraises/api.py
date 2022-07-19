38e402fd0a38f6f58a8dc821807ef2400736fd17
4ugeistr <andrii.dutko@gmail.com>
2022-07-19 20:59:21 +0300

    fixed an import
    - fixed an import in api.py
    - removed an image file from git

--- backend/fundraises/api.py
+++ backend/fundraises/api.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from fundraises.models import Fundraise
-from fundraises.models import FundraiseCategory
+from fundraises.models import Fundraise, FundraiseCategory
 from fundraises.serializers import FundraisesSerializer
 
 class GetFundraises(viewsets.ModelViewSet):
