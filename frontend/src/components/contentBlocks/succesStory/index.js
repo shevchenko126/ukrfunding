e87fd79e54a4a6acfec364d8dbdf859a22c2c91c
DanyuAntonyk <danyu.antonyk@gmail.com>
2022-07-21 15:22:12 +0300

    .

---
+++ frontend/src/components/contentBlocks/succesStory/index.js
@@ -0,0 +1,62 @@
+
+
+import './index.css';
+
+const succesStory = () => {
+    return (
+        <div class="container-xxl row mx-auto container">
+        
+        <div class="d-flex justify-content-between ">
+            <div class="d-grid gap-5">
+                <div class="w-50">
+                    <p class="fw-bold" style="color: rgba(27, 130, 113, 1); ">SUCCESS STORY</p>
+                    <h2 class="fw-bolder">Read They Story With Akcel</h2>
+                    <p class="fs-6 fw-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                </div>
+                <div>
+                    
+                </div>
+                
+            </div>
+            <button type="button" class="rounded d-flex w-25 py-2 px-3 btn my-auto justify-content-between" style="background-color: #1B8271;">
+                <div class="text-white my-auto ">View More</div>
+                <img src="./images/arrow.svg " alt="" class="my-auto">
+            </button>
+        </div>
+        <div class="row ">
+            <div class="col w-50">
+                <img src="./images/placeholder.svg" alt="" class="w-100">
+            </div>
+            <div class="col w-50 d-grid align-content-between flex-wrap">
+                <h3 class="fw-bold">Samcung Okulus Rivt PC - Powered VR  Gaming Headset</h3>
+                <p class="fs-6 fw-light" >Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit au</p>
+                <div>
+                    <h2 class="fw-semibold" style="color: rgba(27, 130, 113, 1); ">$ 24,553,852.24</h2>
+                    <p>Total funded</p>
+                </div>
+                
+
+                <div class="d-flex pe-2">
+                    <img src="./images/user.svg" alt="" >
+                    <div sclass="my-auto ps-3">   
+                        <div class="d-flex gap-2">
+                            <p class="my-auto">Hendric Anderson</p> 
+                            <img src="./images/Vector.svg" alt="">
+                            <img src="./images/Vector.svg" alt="">
+                            <img src="./images/Vector.svg" alt="">
+                        </div>
+                        <div class="d-flex gap-3">
+                            <p class="my-auto">12 Campaign</p>
+                            <img src="./images/dot.svg" alt="">
+                            <p class="my-auto">New York, London</p>
+                        </div>
+                    </div>
+                </div>
+            
+
+            </div>
+        </div>
+    </div>
+	)
+}
+export default succesStory
