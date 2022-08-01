bd94b1f2563aa77e2ed40da8a48fbb179aa85630
DanyloDiachenko <107757498+DanyloDiachenko@users.noreply.github.com>
2022-08-01 17:18:55 +0300

    added block Recent Projects in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Page3block1 from './components/contentBlocks/page3_block1_includeSearch';
+import RecentProjects from './components/contentBlocks/page1_recent_projects';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Page3block1/>
+      <RecentProjects />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
