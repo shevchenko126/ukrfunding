0eb804a42bd336a5bf63f5047b2d5d2a6b9d01dc
DanyloDiachenko <107757498+DanyloDiachenko@users.noreply.github.com>
2022-08-06 21:55:56 +0300

    fixed img sources (Footer)

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -7,24 +7,24 @@ const Footer = () => {
                 <div className="row gx-5 pt-5 mx-0">
                     <div className="col-lg-4 col-md-12 footer__first-col">
                         <div style={{marginLeft: '15%'}} className="mt-4">
-                            <img src="..../public/image/footer_logo.svg" />
+                            <img src="./image/footer_logo.svg" />
                         </div>
                         <div className="footer__first-col__text text-white mt-4">Akcel is a Crowdfunding & Charity Website by Peterdraw lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et</div>
                         <div className="row mt-4 text-center">
                             <div className="col-2" style={{marginLeft: '12%'}}>
-                                <img src="..../public/image/footer_facebook.svg" className="footer__first-col__logo" />
+                                <img src="./image/footer_facebook.svg" className="footer__first-col__logo" />
                             </div>
                             <div className="col-2">
-                                <img src="..../public/image/footer_twitter.svg" className="footer__first-col__logo twitter" />
+                                <img src="./image/footer_twitter.svg" className="footer__first-col__logo twitter" />
                             </div>
                             <div className="col-2">
-                                <img src="..../public/image/footer_youtube.svg" className="footer__first-col__logo" />
+                                <img src="./image/footer_youtube.svg" className="footer__first-col__logo" />
                             </div>
                             <div className="col-2">
-                                <img src="..../public/image/footer_in.svg" className="footer__first-col__logo in" />
+                                <img src="./image/footer_in.svg" className="footer__first-col__logo in" />
                             </div>
                             <div className="col-2">
-                                <img src="..../public/image/footer_instagram.svg" className="footer__first-col__logo" />
+                                <img src="./image/footer_instagram.svg" className="footer__first-col__logo" />
                             </div>
                         </div>
                     </div>
@@ -58,20 +58,20 @@ const Footer = () => {
                         <div>
                             <div className="footer__title mt-4 pb-4">Get in Touch with Us</div>
                             <div className="d-flex">
-                                <img src="..../public/image/footer_location.svg" className="mx-2" style={{marginTop: '-20px'}}/>
+                                <img src="./image/footer_location.svg" className="mx-2" style={{marginTop: '-20px'}}/>
                                 <div className="footer__location">832  Thompson Drive, San Fransisco CA 94107, United States</div>
                             </div>
                             <div className="d-flex mt-4">
-                                <img src="..../public/image/footer_phone.svg" className="mx-2" />
+                                <img src="./image/footer_phone.svg" className="mx-2" />
                                 <div className="footer__location">394-091-3312</div>
                             </div>
                             <div className="d-flex" style={{marginTop: '40px'}}>
-                                <img src="..../public/image/footer_email.svg" className="mx-2" />
+                                <img src="./image/footer_email.svg" className="mx-2" />
                                 <div className="footer__location">support@akcel.com</div>
                             </div>
                         </div>
                     </div>
-                    <hr style={{border: '2px solid #3D3F4E', margiTop: '150px'}} />
+                    <hr style={{border: '2px solid #3D3F4E', marginTop: '150px'}} />
                     <div className="text-white text-center pb-4 pt-4 h6">Akcel Crowdfunding & Charity Website  -   © 2021 by Peterdraw</div>
                 </div>
             </div>
