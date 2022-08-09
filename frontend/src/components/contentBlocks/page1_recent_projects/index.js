228124eb32959728b935fe14d384d07d9d4ca04b
DanyloDiachenko <107757498+DanyloDiachenko@users.noreply.github.com>
2022-08-09 11:40:01 +0300

    recent projects created dynamic rendering

--- frontend/src/components/contentBlocks/page1_recent_projects/index.js
+++ frontend/src/components/contentBlocks/page1_recent_projects/index.js
@@ -1,163 +1,59 @@
 import './index.css';
+import OneProject from './OneProject';
 
 const RecentProjects = () => {
+
+    const projects = [
+        {
+            category: 'medical',
+            title: 'Beautiful color for designers & students',
+            progress: '45%',
+            raised: 'Total Raised $ 5,345',
+            days_left: '45 Days left',
+            author: 'Cheyenne Curtis',
+            campaign: '12 Campaign',
+            location: 'New York, London',
+        },
+        {
+            category: 'design',
+            title: 'Notebook for your creative observation',
+            progress: '30%',
+            raised: 'Total Raised $ 5,345',
+            days_left: '45 Days left',
+            author: 'Ahmad Schleifer',
+            campaign: '12 Campaign',
+            location: 'New York, London',
+        },
+        {
+            category: 'technnology',
+            title: 'VR Ears | Hear Off-World Listen Off-Ear',
+            progress: '90%',
+            raised: 'Total Raised $ 5,345',
+            days_left: '45 Days left',
+            author: 'Skylar Gouse',
+            campaign: '12 Campaign',
+            location: 'New York, London',
+        },
+        {
+            category: 'technnology',
+            title: 'Self Driving Robot for Target Shooting Game',
+            progress: '7%',
+            raised: 'Total Raised $ 5,345',
+            days_left: '45 Days left',
+            author: 'Maren George',
+            campaign: '12 Campaign',
+            location: 'New York, London',
+        },
+    ]
+
     return (
         <div className="container recent_projects">
             <div className="text-center recent_projects__title">Recent Projects</div>
             <div className="mt-4 text-center recent_projects__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</div>
             <div className="row mt-5 mb-5">
-                <div className="col-xl-3 col-md-6">
-                    <div className="mt-5 recent_projects__item">
-                        <div className="recent_projects__item__image"></div>
-                        <div className="m-3 text-uppercase text-success recent_projects__item__category">medical</div>
-                        <div className="m-3 recent_projects__item__theme">Beautiful color for designers & students</div>
-                        <div className="m-3 progress">
-                            <div className="progress-bar" style={{ width: '47%' }}></div>
-                        </div>
-                        <div className="d-flex m-3 mt-4">
-                            <img src="../image/page1_recent_projects_coins.svg" />
-                            <div className="mx-3 recent_projects__item__text">Total Raised $ 5,345</div>
-                        </div>
-                        <div className="d-flex m-3 mt-4">
-                            <img src="../image/page1_recent_projects_calendar.svg" />
-                            <div className="mx-3 recent_projects__item__text">45 Days left</div>
-                        </div>
-                        <div className="recent_projects__item__author pb-4">
-                            <div className="mt-5 row d-flex">
-                                <div className='col-lg-3 col-sm-12'>
-                                    <img src="../image/page1_recent_projects_icon.svg" className="mx-3" />
-                                </div>
-                                <div className='col-lg-9 col-sm-12'>
-                                    <div className="d-flex">
-                                        <div className="mx-2 recent_projects__item__author__author">Skylar Gouse</div>
-                                        <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                        <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                        <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                    </div>
-                                    <div className="d-flex mx-2 mt-3">
-                                        <div className="text-success recent_projects__item__author__campaign">12 Campaign</div>
-                                        <img src="../image/page1_recent_projects_dot.svg" className="mx-3" />
-                                        <div className="recent_projects__item__author__city">New York, London</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-3 col-md-6">
-                    <div className="mt-5 recent_projects__item">
-                        <div className="recent_projects__item__image"></div>
-                        <div className="m-3 text-uppercase text-success recent_projects__item__category">design</div>
-                        <div className="m-3 recent_projects__item__theme">Notebook for your creative observation</div>
-                        <div className="m-3 progress">
-                            <div className="progress-bar" style={{ width: '40%' }}></div>
-                        </div>
-                        <div className="d-flex m-3 mt-4">
-                            <img src="../image/page1_recent_projects_coins.svg" />
-                            <div className="mx-3 recent_projects__item__text">Total Raised $ 5,345</div>
-                        </div>
-                        <div className="d-flex m-3 mt-4">
-                            <img src="../image/page1_recent_projects_calendar.svg" />
-                            <div className="mx-3 recent_projects__item__text">45 Days left</div>
-                        </div>
-                        <div className="recent_projects__item__author pb-4">
-                            <div className="mt-5 row d-flex">
-                                <div className='col-lg-3 col-sm-12'>
-                                    <img src="../image/page1_recent_projects_icon.svg" className="mx-3" />
-                                </div>
-                                <div className='col-lg-9 col-sm-12'>
-                                    <div className="d-flex">
-                                        <div className="mx-2 recent_projects__item__author__author">Skylar Gouse</div>
-                                        <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                        <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                        <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                    </div>
-                                    <div className="d-flex mx-2 mt-3">
-                                        <div className="text-success recent_projects__item__author__campaign">12 Campaign</div>
-                                        <img src="../image/page1_recent_projects_dot.svg" className="mx-3" />
-                                        <div className="recent_projects__item__author__city">New York, London</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-3 col-md-6">
-                    <div className="mt-5 recent_projects__item">
-                        <div className="recent_projects__item__image"></div>
-                        <div className="m-3 text-uppercase text-success recent_projects__item__category">technology</div>
-                        <div className="m-3 recent_projects__item__theme">VR Ears | Hear Off-World Listen Off-Ear</div>
-                        <div className="m-3 progress">
-                            <div className="progress-bar" style={{ width: '85%' }}></div>
-                        </div>
-                        <div className="d-flex m-3 mt-4">
-                            <img src="../image/page1_recent_projects_coins.svg" />
-                            <div className="mx-3 recent_projects__item__text">Total Raised $ 5,345</div>
-                        </div>
-                        <div className="d-flex m-3 mt-4">
-                            <img src="../image/page1_recent_projects_calendar.svg" />
-                            <div className="mx-3 recent_projects__item__text">45 Days left</div>
-                        </div>
-                        <div className="recent_projects__item__author pb-4">
-                            <div className="mt-5 row d-flex">
-                                <div className='col-lg-3 col-sm-12'>
-                                    <img src="../image/page1_recent_projects_icon.svg" className="mx-3" />
-                                </div>
-                                <div className='col-lg-9 col-sm-12'>
-                                    <div className="d-flex">
-                                        <div className="mx-2 recent_projects__item__author__author">Skylar Gouse</div>
-                                        <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                        <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                        <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                    </div>
-                                    <div className="d-flex mx-2 mt-3">
-                                        <div className="text-success recent_projects__item__author__campaign">12 Campaign</div>
-                                        <img src="../image/page1_recent_projects_dot.svg" className="mx-3" />
-                                        <div className="recent_projects__item__author__city">New York, London</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-3 col-md-6">
-                    <div className="mt-5 recent_projects__item">
-                        <div className="recent_projects__item__image"></div>
-                        <div className="m-3 text-uppercase text-success recent_projects__item__category">technology</div>
-                        <div className="m-3 recent_projects__item__theme">Self Driving Robot for Target Shooting Game</div>
-                        <div className="m-3 progress">
-                            <div className="progress-bar" style={{ width: '15%' }}></div>
-                        </div>
-                        <div className="d-flex m-3 mt-4">
-                            <img src="../image/page1_recent_projects_coins.svg" />
-                            <div className="mx-3 recent_projects__item__text">Total Raised $ 5,345</div>
-                        </div>
-                        <div className="d-flex m-3 mt-4">
-                            <img src="../image/page1_recent_projects_calendar.svg" />
-                            <div className="mx-3 recent_projects__item__text">45 Days left</div>
-                        </div>
-                        <div className="recent_projects__item__author pb-4">
-                            <div className="mt-5 row d-flex">
-                                <div className='col-lg-3 col-sm-12'>
-                                    <img src="../image/page1_recent_projects_icon.svg" className="mx-3" />
-                                </div>
-                                <div className='col-lg-9 col-sm-12'>
-                                    <div className="d-flex">
-                                        <div className="mx-2 recent_projects__item__author__author">Skylar Gouse</div>
-                                        <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                        <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                        <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                    </div>
-                                    <div className="d-flex mx-2 mt-3">
-                                        <div className="text-success recent_projects__item__author__campaign">12 Campaign</div>
-                                        <img src="../image/page1_recent_projects_dot.svg" className="mx-3" />
-                                        <div className="recent_projects__item__author__city">New York, London</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {projects.map((project) => (
+                    <OneProject project={project} />
+                ))}
             </div>
         </div>
     )
