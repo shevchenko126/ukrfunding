49392cde3ee7d7a36a2575fbc8f2d3d74e2fdfc0
DanyloDiachenko <107757498+DanyloDiachenko@users.noreply.github.com>
2022-08-18 13:15:34 +0300

    cerated content block stories (page 10)

--- frontend/src/components/contentBlocks/page10_stories/index.jsx
+++ frontend/src/components/contentBlocks/page10_stories/index.jsx
@@ -16,11 +16,11 @@ const Stories = () => {
                     </div>
                     <article className='success_story_main'>
                         <div className='row mt-4'>
-                            <div className="col-xl-5 col-md-12 mt-5 text-center">
+                            <div className="col-lg-5 col-md-12 mt-5 d-flex align-items-center justify-content-center">
                                 <img src='./image/page10_profile.svg' className='img-fluid' />
                             </div>
-                            <div className="col-xl-7 col-md-12 mt-5 border success_story_main__right">
-                                <div className='black success_story_main__right__title' style={{marginTop: '35px'}}>Samcung Okulus Rivt PC - Powered VR  Gaming Headset</div>
+                            <div className="col-lg-7 col-md-12 mt-5 success_story_main__right">
+                                <div className='black success_story_main__right__title' style={{ marginTop: '35px' }}>Samcung Okulus Rivt PC - Powered VR  Gaming Headset</div>
                                 <div className='black mt-4 success_story_main__right__text'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit au</div>
                                 <div className='green mt-4 success_story_main__right__number'>$ 24,553,852.24</div>
                                 <div className='black mt-3 success_story_main__right__number-text'>Total funded</div>
@@ -29,6 +29,68 @@ const Stories = () => {
                                     <div>
                                         <div className='d-flex'>
                                             <div className='mx-3 black success_story_main__right__author__author'>Hendric Anderson</div>
+                                            <img src='./image/page5_project_star.svg' />
+                                            <img src='./image/page5_project_star.svg' className='px-1' />
+                                            <img src='./image/page5_project_star.svg' />
+                                        </div>
+                                        <div className='d-flex mx-3 mt-2'>
+                                            <div className='green success_story_main__right__author__campaign'>12 Campaign</div>
+                                            <img src='./image/page_8_dot.svg' className='mx-3' />
+                                            <div className='success_story_main__right__author__location'>New York, London</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <div className='container success-story_main'>
+                    <article className='success_story_main'>
+                        <div className='row mt-4'>
+                            <div className="col-lg-7 col-md-12 mt-5 success_story_main__right">
+                                <div className='black success_story_main__right__title' style={{ marginTop: '35px' }}>Super Bass Headset by Beast Monster Technology Development</div>
+                                <div className='black mt-4 success_story_main__right__text'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit au</div>
+                                <div className='green mt-4 success_story_main__right__number'>$ 9,425,552.43</div>
+                                <div className='black mt-3 success_story_main__right__number-text'>Total funded</div>
+                                <div className='d-flex mt-5 success_story_main__right__author'>
+                                    <img src='./image/page10_author.svg' />
+                                    <div>
+                                        <div className='d-flex'>
+                                            <div className='mx-3 black success_story_main__right__author__author'>Bella Simalungun</div>
+                                            <img src='./image/page5_project_star.svg' />
+                                            <img src='./image/page5_project_star.svg' className='px-1' />
+                                            <img src='./image/page5_project_star.svg' />
+                                        </div>
+                                        <div className='d-flex mx-3 mt-2'>
+                                            <div className='green success_story_main__right__author__campaign'>12 Campaign</div>
+                                            <img src='./image/page_8_dot.svg' className='mx-3' />
+                                            <div className='success_story_main__right__author__location'>New York, London</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-5 col-md-12 mt-5 d-flex align-items-center justify-content-center">
+                                <img src='./image/page10_profile.svg' className='img-fluid' />
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <div className='container success-story_main'>
+                    <article className='success_story_main'>
+                        <div className='row mt-4'>
+                            <div className="col-lg-5 col-md-12 mt-5 d-flex align-items-center justify-content-center">
+                                <img src='./image/page10_profile.svg' className='img-fluid' />
+                            </div>
+                            <div className="col-lg-7 col-md-12 mt-5 success_story_main__right">
+                                <div className='black success_story_main__right__title' style={{ marginTop: '35px' }}>Mega Drone Stabilizier</div>
+                                <div className='black mt-4 success_story_main__right__text'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit au</div>
+                                <div className='green mt-4 success_story_main__right__number'>$ 79,412,552.90</div>
+                                <div className='black mt-3 success_story_main__right__number-text'>Total funded</div>
+                                <div className='d-flex mt-5 success_story_main__right__author'>
+                                    <img src='./image/page10_author.svg' />
+                                    <div>
+                                        <div className='d-flex'>
+                                            <div className='mx-3 black success_story_main__right__author__author'>Dedi Cahyadi</div>
                                             <img src='./image/page5_project_star.svg' />
                                             <img src='./image/page5_project_star.svg' className='px-1' />
                                             <img src='./image/page5_project_star.svg' />
