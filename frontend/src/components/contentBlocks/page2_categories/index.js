929f4ea722c39e2b27f1067a740c9833c36ccb5c
DanyloDiachenko <107757498+DanyloDiachenko@users.noreply.github.com>
2022-09-08 22:24:32 +0300

    improving code

--- frontend/src/components/contentBlocks/page2_categories/index.js
+++ frontend/src/components/contentBlocks/page2_categories/index.js
@@ -1,8 +1,32 @@
 ﻿import './index.css';
 
 const Page2Categories = () => {
+
+	const categories = [
+		{
+			img: '../image/page2_categories_icon-head.svg',
+			title: 'Quo maxime qui impedit odio soluta',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		},
+		{
+			img: '../image/page2_categories_icon-square.svg',
+			title: 'Ut nisi ea vero itaque error magnam',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		},
+		{
+			img: '../image/page2_categories_icon-medical_bag.svg',
+			title: 'Quaerat nobis est',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		},
+		{
+			img: '../image/page2_categories_icon-book.svg',
+			title: 'Deleniti iure ipsa eos eaque unde',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		},
+	]
+
 	return (
-		<div className='container' style={{maxWidth: '1620px'}}>
+		<div className='container' style={{ maxWidth: '1620px' }}>
 			<div className="text-center p-0 categories">
 				<div className="categories__header">CATEGORIES</div>
 				<div className="categories__title">Why choose Akcel</div>
@@ -10,47 +34,20 @@ const Page2Categories = () => {
 			</div>
 			<div className="p-0 categories-main">
 				<div className="row d-flex justify-content-between">
-					<div className="col-lg-6 col-md-12"> 
-						<div className="d-flex categories-main__part mt-5">
-							<img src="../image/page2_categories_icon-head.svg" className="categories-main__part__logo" />
-							<div style={{marginLeft: '3%'}}>
-								<div className="categories-main__part__title">Quo maxime qui impedit odio soluta</div>
-								<div className="categories-main__part__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+					{categories.map(({ img, title, description }) =>
+						<div className="col-lg-6 col-md-12">
+							<div className="d-flex categories-main__part mt-5">
+								<img src={img} className="categories-main__part__logo" />
+								<div style={{ marginLeft: '3%' }}>
+									<div className="categories-main__part__title">{title}</div>
+									<div className="categories-main__part__text">{description}</div>
+								</div>
 							</div>
 						</div>
-					</div>		
-					<div className="col-lg-6 col-md-12"> 
-						<div className="d-flex categories-main__part mt-5">
-							<img src="../image/page2_categories_icon-square.svg" className="categories-main__part__logo" />
-							<div style={{marginLeft: '3%'}}>
-								<div className="categories-main__part__title">Ut nisi ea vero itaque error magnam</div>
-								<div className="categories-main__part__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="row d-flex justify-content-between">
-					<div className="col-lg-6 col-md-12">
-						<div className="d-flex categories-main__part mt-5">
-							<img src="../image/page2_categories_icon-medical_bag.svg" className="categories-main__part__logo" />
-							<div style={{marginLeft: '3%'}}>
-								<div className="categories-main__part__title">Quaerat nobis est</div>
-								<div className="categories-main__part__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
-							</div>
-						</div>
-					</div>
-					<div className="col-lg-6 col-md-12">
-						<div className="d-flex categories-main__part mt-5">
-							<img src="../image/page2_categories_icon-book.svg" className="categories-main__part__logo" />
-							<div style={{marginLeft: '3%'}}>
-								<div className="categories-main__part__title">Deleniti iure ipsa eos eaque unde</div>
-								<div className="categories-main__part__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
-							</div>
-						</div>
-					</div>
+					)}
 				</div>
 			</div>
-		</div>		
+		</div>
 	)
 }
 
