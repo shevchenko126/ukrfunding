2cc47c236ebe6104ea82d953e24f40bdb62a8ab5
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-30 11:36:49 +0200

    _

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -3,68 +3,68 @@ import './index.css';
 const Header = () => {
     return (
         <section>
-            <div className='container header-top'>
-                <div className='row header__top'>
-                    <div className='col-lg-7 col-md-12 header__top__left px-3'>
-                        <div className='row'>
-                            <div className='col-lg-6 col-md-12 mt-4 header__top__left__text'>
-                                <div>Welcome to Akcel, Crowdfunding & Charity Agency</div>
+            <div className='container'>
+                <div className='row d-flex align-items-center'>
+                    <div className='col-lg-7 col-md-12 px-3'>
+                        <div className='row align-items-center'>
+                            <div className='col-lg-6 col-md-12 mt-4'>
+                                <h6>Welcome to Akcel, Crowdfunding & Charity Agency</h6>
                             </div>
                             <div className='col-lg-6 col-md-12 mt-3'>
                                 <a href="/register">
-                                    <button className='text-white header__top__left__button d-flex align-items-center justify-content-center'>
-                                        <img src="./image/header_top_singAp.svg" />
+                                    <button className='button-bg-green-text-white d-flex align-items-center justify-content-center'>
+                                        <img alt="sign up" src="./image/header_top_singAp.svg" />
                                         <div className='mx-2'>Sign Up Now</div>
                                     </button>
                                 </a>
                             </div>
                         </div>
                     </div>
-                    <div className='col-lg-5 col-md-12 header__top__right'>
+                    <div className='col-lg-5 col-md-12'>
                         <div className='row'>
-                            <div className='col-lg-6 col-md-12 mt-4 d-flex header__top__right__phone-email'>
-                                <img src="./image/header_top_phone.svg" />
-                                <div className='px-3'>394-091-3312</div>
+                            <div className='col-lg-6 col-md-12 mt-4 d-flex header-top-email-phone'>
+                                <img src="./image/header_top_phone.svg" alt="phone" />
+                                <h6 className='px-3 mb-0'>394-091-3312</h6>
                             </div>
-                            <div className='col-lg-6 col-md-12 mt-4 d-flex header__top__right__phone-email'>
-                                <img src="./image/header_top_email.svg" />
-                                <div style={{ marginLeft: '15px' }}>support@akcel.com</div>
+                            <div className='col-lg-6 col-md-12 mt-4 d-flex header-top-email-phone'>
+                                <img src="./image/header_top_email.svg" alt="email" />
+                                <h6 className='mb-0' style={{ marginLeft: '15px' }}>support@akcel.com</h6>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <hr style={{ width: '100%', color: 'rgba(0, 0, 0, 0.5)' }} className='mt-4' />
-            <div className='container mt-4 header-bottom pb-4'>
+            <div className='container mt-4 pb-4'>
                 <div className='row d-flex'>
-                    <div className='col-lg-3 col-md-12 header-bottom__logo'>
-                        <img src="./image/logo_Akcel.png" />
-                    </div>
+                    <a href="/" className='col-lg-3 col-md-12'>
+                        <img src="./image/logo_Akcel.png" alt="logo" />
+                    </a>
                     <div className='col-lg-6'>
                         <div className='row text-center d-flex justify-content-center align-items-center'>
-                            <div className='col mt-4'>
+                            <h5 className='col mt-4'>
                                 <a href='/' className='header-bottom__ref'>Home</a>
-                            </div>
-                            <div className='col mt-4'>
+                            </h5>
+                            <h5 className='col mt-4'>
                                 <a href='/about-us' className='header-bottom__ref'>About</a>
-                            </div>
-                            <div className='col mt-4'>
+                            </h5>
+                            <h5 className='col mt-4'>
                                 <div className='d-flex justify-content-center'>
                                     <a href='/' className='header-bottom__ref'>Projects</a>
-                                    {/* <img src='./image/header_down_arrow.svg' /> */}
+                                    <img className='mx-2' alt="more" src='./image/header_down_arrow.svg' />
                                 </div>
-                            </div>
-                            <div className='col mt-4'>
+                            </h5>
+                            <h5 className='col mt-4'>
                                 <a href='/latest-news' className='header-bottom__ref'>News</a>
-                            </div>
-                            <div className='col mt-4'>
+                            </h5>
+                            <h5 className='col mt-4'>
                                 <a href='/conact-us' className='header-bottom__ref'>Contact Us</a>
-                            </div>
+                            </h5>
                         </div>
                     </div>
                     <div className='col-lg-3 col-md-12 mt-2 d-flex header-bottom__button'>
-                        <button className='header-bottom__button__button'>
-                            <img src='../image/header_bottom_user.svg' />
+                        <button className='button-bg-white-text-pink'>
+                            <img alt="user" src='../image/header_bottom_user.svg' />
                             <span className='mx-3'>My Account</span>
                         </button>
                     </div>
