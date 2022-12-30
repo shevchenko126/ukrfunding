d88c389cc3da013309a7bfbd73bdc56758a38a30
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-30 13:23:06 +0200

    _

--- frontend/src/components/contentBlocks/page1_success_story/index.js
+++ frontend/src/components/contentBlocks/page1_success_story/index.js
@@ -17,44 +17,44 @@ const SuccessStory = () => {
         <div className="container success_story">
             <div className="row pb-5">
                 <div className="col-lg-6 col-md-12">
-                    <div className="text-uppercase text-success success_story__title">Success story</div>
-                    <div className="mt-4 success_story__theme">Read The Story With Akcel</div>
-                    <div className="mt-4 success_story__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</div>
+                    <h4 className="text-uppercase text-success">Success story</h4>
+                    <h2 className="mt-4">Read The Story With Akcel</h2>
+                    <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
                 </div>
                 <div className="col-lg-6 col-md-12 d-flex align-items-center success_story__button-col">
-                    <button className="mt-5 success_story__button">
-                        <div className="d-flex">
-                            <div className="text-white px-3 mx-2 success_story__button__text">View More</div>
-                            <img src="../image/page1_success_story_arrow.svg" className="mx-5" alt="" />
-                        </div>
+                    <button className="mt-5 button-bg-green-text-white">
+                        <span className="d-flex">
+                            <div className="text-white">View More</div>
+                            <img src="../image/page1_success_story_arrow.svg" style={{paddingLeft: '20px'}} alt=">" />
+                        </span>
                     </button>
                 </div>
             </div>
-            <div className="row mt-5 success_story__main">
-                <div className="col-lg-6 col-md-12 success_story__main__column">
-                    <img src="../image/page1_success_story_placeholder.svg" className="success_story__main__column__photo" alt="" />
+            <div className="row mt-5 d-flex align-items-center">
+                <div className="col-lg-6 col-md-12">
+                    <img src="../image/page1_success_story_placeholder.svg" style={{width: '100%'}} alt="#" />
                 </div>
                 <div className="col-lg-6 col-md-12">
-                    <div className="success_story__main__title">{data.title}</div>
-                    <div className="mt-4 success_story__main__text">{data.description}</div>
-                    <div className="mt-4 text-success success_story__main__number">{data.totalFounded}</div>
-                    <div className="mt-2 success_story__main__number-text">Total funded</div>
+                    <h3 className="">{data.title}</h3>
+                    <p className="mt-4">{data.description}</p>
+                    <h2 className="mt-4 text-success">{data.totalFounded}</h2>
+                    <h5 className="mt-2 success_story__main__number-text">Total funded</h5>
                     <div className="mt-5 success_story__main__author">
                         <div className="row d-flex">
                             <div className='col-lg-1 col-sm-12'>
-                                <img src="../image/page1_success_story_user.svg" alt="" />
+                                <img src="./image/page1_success_story_user.svg" alt="user" />
                             </div>
-                            <div className='col-lg-11 col-sm-12 success_story__main__author-author'>
+                            <div className='col-lg-11 col-sm-12'>
                                 <div className="d-flex">
-                                    <div className="success_story__main__author__author">{data.author.name}</div>
-                                    <img src="../image/page1_success_story_Vector.svg" alt="" className="mx-1" />
-                                    <img src="../image/page1_success_story_Vector.svg" alt="" className="mx-1" />
-                                    <img src="../image/page1_success_story_Vector.svg" alt="" className="mx-1" />
+                                    <h5>{data.author.name}</h5>
+                                    <img src="./image/page1_success_story_Vector.svg" alt="#" className="mx-1" />
+                                    <img src="./image/page1_success_story_Vector.svg" alt="#" className="mx-1" />
+                                    <img src="./image/page1_success_story_Vector.svg" alt="#" className="mx-1" />
                                 </div>
                                 <div className="mt-2 d-flex">
-                                    <div className="text-success success_story__main__author__campaign">{data.author.campaign} Campaign</div>
+                                    <div className="text-success author-campaign">{data.author.campaign} Campaign</div>
                                     <img src="/image/page1_success_story_dot.svg" className="mx-3" alt="" />
-                                    <div className="success_story__main__author__city">{data.author.city}</div>
+                                    <div className="author-city">{data.author.city}</div>
                                 </div>
                             </div>
                         </div>
