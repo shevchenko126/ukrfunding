cbcb882cd2a2dee0dcaad91d8e900439a1fb915d
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-30 13:30:16 +0200

    _

--- frontend/src/components/contentBlocks/page1_our_partnership/index.js
+++ frontend/src/components/contentBlocks/page1_our_partnership/index.js
@@ -1,25 +1,34 @@
 ﻿import './index.css';
 
 const OurPartnership = () => {
+
+	const partners = [
+		{
+			src: './image/page_1_our_partnership_highlow.svg'
+		},
+		{
+			src: './image/page_1_our_partnership_boost.svg'
+		},
+		{
+			src: './image/page_1_our_partnership_emajine.svg'
+		},
+		{
+			src: './image/page_1_our_partnership_manthul.svg'
+		},
+		{
+			src: './image/page_1_our_partnership_glowup.svg'
+		},
+	]
+
 	return (
 		<div className="container logotypes text-center">
-		<div className="mt-5 logotypes__title">Our Partnership</div>
-			<div className="row text-center">
-				<div className="col mt-5">
-					<img src="../image/page_1_our_partnership_highlow.svg" />
-				</div>
-				<div className="col mt-5">
-					<img src="../image/page_1_our_partnership_boost.svg" /> 
-				</div>
-				<div className="col mt-5">
-					<img src="../image/page_1_our_partnership_emajine.svg" />
-				</div>
-				<div className="col mt-5">
-					<img src="../image/page_1_our_partnership_manthul.svg" />
-				</div>
-				<div className="col mt-5">
-					<img src="../image/page_1_our_partnership_glowup.svg" />
-				</div>
+			<h3>Our Partnership</h3>
+			<div className="row">
+				{partners.map(({ src }) => (
+					<div className="col mt-5">
+						<img src={src} alt="partner" />
+					</div>
+				))}
 			</div>
 		</div>
 	)
