913630c963bbd2d6f1669c0238e1b8f76d24f557
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-30 12:48:43 +0200

    _

--- frontend/src/components/contentBlocks/page1_we_help_surface/index.jsx
+++ frontend/src/components/contentBlocks/page1_we_help_surface/index.jsx
@@ -12,12 +12,12 @@ const WeHelpSurface = () => {
                     <h1 className="mt-3">We Help Surface</h1>
                     <h2 className="mt-1">Innovations In Technology</h2>
                     <p className="mt-4 we_help_surface__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
-                    <div className="d-flex row pb-5 we_help_surface__buttons">
+                    <div className="d-flex row pb-5">
                         <div className='col-lg-4 col-md-12'>
-                            <button className="mt-5 text-white we_help_surface__buttons__learn_more">
+                            <button className="mt-5 button-bg-pink-text-white">
                                 <div className="d-flex">
-                                    <div className="px-3">Learn More</div>
-                                    <img src="../image/page1_we_help_surface_arrow.svg" className="mx-4" alt="" />
+                                    <div style={{paddingRight: '15px'}}>Learn More</div>
+                                    <img src="../image/page1_we_help_surface_arrow.svg" alt="" />
                                 </div>
                             </button>
                         </div>
@@ -25,7 +25,7 @@ const WeHelpSurface = () => {
                             <button className="mt-5 button-bg-white-text-gray">Donate</button>
                         </div>
                     </div>
-                    <div className="mt-5 we_help_surface__our_partners">Our Partners</div>
+                    <h5 className="mt-5 we_help_surface__our_partners">Our Partners</h5>
                     <div className="mt-1 text-center row we_help_surface__logotypes">
                         <div className="col">
                             <img src="../image/page1_we_help_surface_highlow.svg" className="mt-3" alt="" />
@@ -39,7 +39,7 @@ const WeHelpSurface = () => {
                     </div>
                 </div>
                 <div className="col-lg-6 col-md-12 d-flex justify-content-center">
-                    <img src="../image/page1_we_help_surface_panels.svg" className="img-fluid mt-5" alt="" />
+                    <img src="./image/page1_we_help_surface_panels.svg" className="img-fluid mt-5" alt="" />
                 </div>
             </div>
         </div>
