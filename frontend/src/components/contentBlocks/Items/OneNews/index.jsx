019dd054a1698c35eca47eec6ebaf72dd31a4dfa
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-30 15:52:55 +0200

    _

--- frontend/src/components/contentBlocks/Items/OneNews/index.jsx
+++ frontend/src/components/contentBlocks/Items/OneNews/index.jsx
@@ -1,30 +1,30 @@
 const OneNews = ({ news }) => {
     return (
-        <div className="col mt-5 d-flex justify-content-center p-0">
-            <div className='aksel_news-main-item'>
+        <article className="col-xxl-4 mt-5 d-flex justify-content-center p-0">
+            <div className='item'>
                 <div className="aksel_news-main__logo">
                     <img alt="#" src="../image/page_1_news_profile.svg" style={{ width: '100%' }} />
                     <div className="text-uppercase aksel_news-main__category">{news.category}</div>
                 </div>
-                <a href='#a' className='aksel_news-main-item-href'>
-                    <div className="aksel_news-main__title">{news.title}</div>
+                <a href='#a' className='item-title'>
+                    <h4 className="aksel_news-main__title">{news.title}</h4>
                 </a>
-                <div className="aksel_news-main__text">{news.description}</div>
+                <p className="aksel_news-main__text">{news.description}</p>
                 <div className="mb-5 aksel_news-main__author row d-flex">
                     <div className='col-lg-2 col-sm-12 p-0'>
                         <img alt="#" src="../image/page_1_news_author-profile.svg" />
                     </div>
-                    <div className="col-lg-10 col-sm-12 aksel_news-main__author__author">
-                        <div className="aksel_news-main__author__author__name">{news.author}</div>
+                    <div className="col-lg-10 col-sm-12">
+                        <h5>{news.author}</h5>
                         <div className="d-flex" style={{ marginTop: '15px' }}>
-                            <div className="aksel_news-main__author__time-date">{news.time_read}</div>
+                            <p className="aksel_news-main__author__time-date">{news.time_read}</p>
                             <img alt="#" src="../image/page_1_news_dot.svg" style={{ marginLeft: '6px', marginRight: '6px' }} />
-                            <div className="aksel_news-main__author__time-date">{news.date}</div>
+                            <p className="aksel_news-main__author__time-date">{news.date}</p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </article>
     )
 }
 
