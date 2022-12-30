b622f199c5eefbdd8a43a37425b298333688e39a
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-30 13:48:31 +0200

    _

--- frontend/src/components/contentBlocks/page1_recent_projects/index.jsx
+++ frontend/src/components/contentBlocks/page1_recent_projects/index.jsx
@@ -56,37 +56,37 @@ const RecentProjects = () => {
                     <div className="col-xl-3 col-md-6">
                         <div className="mt-5 item" style={{ maxWidth: '380px' }}>
                             <div className="recent_projects__item__image"></div>
-                            <div className="m-3 text-uppercase text-success recent_projects__item__category">{project.category}</div>
-                            <div className="m-3">
-                                <a href='#' className="recent_projects__item__theme">{project.title}</a>
-                            </div>
+                            <div className="m-3 text-success item-category">{project.category}</div>
+                            <h4 className="m-3">
+                                <a href='/' className="item-title">{project.title}</a>
+                            </h4>
                             <div className="m-3 progress">
                                 <div className="progress-bar" style={{ width: project.progress }}></div>
                             </div>
                             <div className="d-flex m-3 mt-4">
-                                <img src="../image/page1_recent_projects_coins.svg" />
-                                <div className="mx-3 recent_projects__item__text">{project.raised}</div>
+                                <img alt="" src="../image/page1_recent_projects_coins.svg" />
+                                <p className="mx-3 my-0">{project.raised}</p>
                             </div>
-                            <div className="d-flex m-3 mt-4">
-                                <img src="../image/page1_recent_projects_calendar.svg" />
-                                <div className="mx-3 recent_projects__item__text">{project.days_left}</div>
+                            <div className="d-flex align-items-center m-3 mt-4">
+                                <img alt="" src="../image/page1_recent_projects_calendar.svg" />
+                                <p className="mx-3 my-0">{project.days_left}</p>
                             </div>
-                            <div className="recent_projects__item__author pb-4">
+                            <div className="pb-4">
                                 <div className="row d-flex">
                                     <div className='col-lg-2 col-sm-12 mt-4'>
-                                        <img src="../image/page1_recent_projects_icon.svg" className="mx-3" />
+                                        <img alt="" src="../image/page1_recent_projects_icon.svg" className="mx-3" />
                                     </div>
                                     <div className='col-lg-10 col-sm-12 mt-4'>
                                         <div className="d-flex">
-                                            <div className="mx-4 recent_projects__item__author__author">{project.author}</div>
-                                            <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                            <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
-                                            <img src="../image/page1_recent_projects_star.svg" className="mx-1" />
+                                            <h5 className="mx-4">{project.author}</h5>
+                                            <img alt="" src="../image/page1_recent_projects_star.svg" className="mx-1" />
+                                            <img alt="" src="../image/page1_recent_projects_star.svg" className="mx-1" />
+                                            <img alt="" src="../image/page1_recent_projects_star.svg" className="mx-1" />
                                         </div>
                                         <div className="d-flex mx-4 mt-3">
-                                            <div className="text-success recent_projects__item__author__campaign">{project.campaign}</div>
-                                            <img src="../image/page1_recent_projects_dot.svg" className="mx-3" />
-                                            <div className="recent_projects__item__author__city">{project.location}</div>
+                                            <div className="text-success author-campaign">{project.campaign}</div>
+                                            <img alt="" src="../image/page1_recent_projects_dot.svg" className="mx-3" />
+                                            <div className="author-city">{project.location}</div>
                                         </div>
                                     </div>
                                 </div>
