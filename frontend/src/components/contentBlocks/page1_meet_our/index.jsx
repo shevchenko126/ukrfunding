fb519485c636a42298bd87cd1fa3dc9b5f7d3b0d
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-30 13:09:41 +0200

    _

---
+++ frontend/src/components/contentBlocks/page1_meet_our/index.jsx
@@ -0,0 +1,50 @@
+﻿import './index.css';
+
+const TeamMember = () => {
+
+	const team = [
+		{
+			img: '../image/page_1_team_member_profile.svg',
+			name: 'Kaylynn Donin',
+			job: 'Managing Director'
+		},
+		{
+			img: '../image/page_1_team_member_profile.svg',
+			name: 'Lindsey Botosh',
+			job: 'National Web Assistant'
+		},
+		{
+			img: '../image/page_1_team_member_profile.svg',
+			name: 'Phillip Schleifer',
+			job: 'National Intranet Specialist'
+		},
+		{
+			img: '../image/page_1_team_member_profile.svg',
+			name: 'Jakob Ekstrom Bothman',
+			job: 'District Accounts Analyst'
+		},
+	]
+
+	return (
+		<div className='team_members container'>
+			<div className='team_members__text text-center mx-auto'>
+				<h4 className="mt-4 text-uppercase text-success">team member</h4>
+				<h2>Meet Our Great Member</h2>
+				<p className='mt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+			</div>
+			<div>
+				<div className="row">
+					{team.map(({ img, name, job }) =>
+						<div className="col-lg-3 col-md-6 col-sm-12 text-center mx-auto mt-4">
+							<img alt="profile" src={img} className="team_members__main__profile" />
+							<div className='team-member-name mt-4'>{name}</div>
+							<p className="team-member-job">{job}</p>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default TeamMember;
