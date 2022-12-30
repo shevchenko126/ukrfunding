c3963ab73d8bf2c491a5ffdc274772d8dd7ffbb1
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-30 19:57:41 +0200

    _

--- frontend/src/components/contentBlocks/page2Fundraising/index.jsx
+++ frontend/src/components/contentBlocks/page2Fundraising/index.jsx
@@ -11,7 +11,7 @@ const Fundraising = () => {
 					</div>
 					<div className="col-lg-7 px-4 mt-5 d-flex align-items-center">
 						<div>
-							<h2>Fundraising is the gentle art of teaching the joy of giving</h2>
+							<h2 className='fundraising-title'>Fundraising is the gentle art of teaching the joy of giving</h2>
 							<p className="my-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
 							<div>
 								<div>
