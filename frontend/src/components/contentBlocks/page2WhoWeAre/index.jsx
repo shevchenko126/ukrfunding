a875cbdd554974738c8d43ba52f0fba671878732
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-30 20:00:02 +0200

    _

--- frontend/src/components/contentBlocks/page2WhoWeAre/index.jsx
+++ frontend/src/components/contentBlocks/page2WhoWeAre/index.jsx
@@ -10,7 +10,7 @@ const WhoWeAre = () => {
 						<h4 className="text-success mx-3">Who We Are</h4>
 					</div>
 					<div style={{ maxWidth: '669px' }}>
-						<h2 className="mb-4">Experienced more than 25 Years in Crowdfunding</h2>
+						<h2 className="mb-4 who-we-are-title">Experienced more than 25 Years in Crowdfunding</h2>
 						<p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
 					</div>
 					<div className="item" style={{ maxWidth: '802px' }}>
@@ -27,9 +27,9 @@ const WhoWeAre = () => {
 					<div className="mt-5 mb-5 item" style={{ maxWidth: '802px' }}>
 						<div className="d-flex">
 							<div>
-								<img alt="#" src="../image/page2_who_we_are_trust.svg" className="mt-4 who_we_are__item__icon" />
+								<img alt="#" src="../image/page2_who_we_are_trust.svg" className="mt-4" />
 							</div>
-							<div className="mt-4 who_we_are__item__right">
+							<div className="mt-4">
 								<div className="who-title">Responsibility</div>
 								<p className="mt-2 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
 							</div>
