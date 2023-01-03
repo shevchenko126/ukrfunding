4734e25af3a4eabc04ace60c1773516edb24fc8f
Danylo Diachenko <danildiachenko23@gmail.com>
2023-01-03 19:08:29 +0200

    _

--- frontend/src/components/contentBlocks/BecomeOurPremiumPartner/index.jsx
+++ frontend/src/components/contentBlocks/BecomeOurPremiumPartner/index.jsx
@@ -2,11 +2,11 @@ import './index.css';
 
 const BecomeOurPremPartner = () => {
     return (
-        <div className='container-fluid become_our_prem' style={{backgroundImage: 'url(/image/page6_background.png'}}>
-            <div className='row align-items-center become_our_prem__row'>
-                <div className='col-lg-7 col-md-12 text-white become_our_prem__row__text'>Become our Premium Partner now!</div>
-                <div className='col-lg-5 col-md-12 d-flex become_our_prem__row-btn'>
-                    <button className='text-white text-uppercase become_our_prem__row__button'>join now</button>
+        <div className='container-fluid become-partner' style={{backgroundImage: 'url(/image/page6_background.png'}}>
+            <div className='row align-items-center become-partner-content container'>
+                <h2 className='col-lg-7 col-md-12 text-white'>Become our Premium Partner now!</h2>
+                <div className='col-lg-5 col-md-12 d-flex become-partner-btn-col'>
+                    <button className='text-white text-uppercase become-partner-btn'>join now</button>
                 </div>
             </div>
         </div>
