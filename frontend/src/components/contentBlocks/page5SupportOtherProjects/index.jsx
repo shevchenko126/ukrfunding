76a81fcd9c6f108976195d350998f06985e68ac9
Danylo Diachenko <danildiachenko23@gmail.com>
2023-01-03 14:09:07 +0200

    _

---
+++ frontend/src/components/contentBlocks/page5SupportOtherProjects/index.jsx
@@ -0,0 +1,68 @@
+﻿import './index.css';
+
+const Support = () => {
+	return (
+		<div className='container mt-5 support_others_projects'>
+			<h3 className='pt-4 text-center'>Support others project</h3>
+			<div className='mt-4 support-img'></div>
+			<h4 className='mt-4' style={{ maxWidth: '404px', margin: 'auto' }}>
+				<a href='' className='project-support'>Gen3 Airoha Air Pro 3 TWS Cancel Headphone</a>
+			</h4>
+			<p style={{ maxWidth: '404px' }} className='mt-4 mx-auto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+			<div className='row' style={{ maxWidth: '404px', margin: 'auto' }}>
+				<div className="row">
+					<div className="col-xl-2 col-lg-12 p-0 mt-4">
+						<img src="/image/page5_support_profile.svg" />
+					</div>
+					<div className="col-xl-9 col-lg-12 p-0 mt-4 author-right">
+						<div className="d-flex">
+							<div className="author-name">Gustavo Mango</div>
+							<img src="/image/page5_support_star.svg" style={{ marginLeft: '10px' }} />
+							<img src="/image/page5_support_star.svg" className="px-1" />
+							<img src="/image/page5_support_star.svg" />
+						</div>
+						<div class="d-flex mt-2">
+							<div className="text-success author-campaign">12 Campaign</div>
+							<img src="/image/page5_support_dot.svg" className="mx-3" />
+							<div className="author-city">New York, London</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='d-flex justify-content-center'>
+				<button className='text-white mt-4 support-btn'>Fund This Project</button>
+			</div>
+			<hr style={{ width: '100%', color: 'rgba(0, 0, 0, 0.5' }} className='mt-4' />
+			<div className='mt-4 support-img'></div>
+			<h4 className='mt-4' style={{ maxWidth: '404px', margin: 'auto' }}>
+				<a href='' className='project-support'>Gen3 Airoha Air Pro 3 TWS Cancel Headphone</a>
+			</h4>
+			<p style={{ maxWidth: '404px' }} className='mt-4 mx-auto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+			<div className='row' style={{ maxWidth: '404px', margin: 'auto' }}>
+				<div className="row">
+					<div className="col-xl-2 col-lg-12 p-0 mt-4">
+						<img src="/image/page5_support_profile.svg" />
+					</div>
+					<div className="col-xl-9 col-lg-12 p-0 mt-4 author-right">
+						<div className="d-flex">
+							<div className="author-name">Gustavo Mango</div>
+							<img src="/image/page5_support_star.svg" style={{ marginLeft: '10px' }} />
+							<img src="/image/page5_support_star.svg" className="px-1" />
+							<img src="/image/page5_support_star.svg" />
+						</div>
+						<div class="d-flex mt-2">
+							<div className="text-success author-campaign">12 Campaign</div>
+							<img src="/image/page5_support_dot.svg" className="mx-3" />
+							<div className="author-city">New York, London</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='d-flex justify-content-center pb-4'>
+				<button className='text-white mt-4 support-btn'>Fund This Project</button>
+			</div>
+		</div>
+	);
+};
+
+export default Support;
