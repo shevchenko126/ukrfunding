14b370674c02332942f333a51debe2576e4e0116
Danylo Diachenko <danildiachenko23@gmail.com>
2023-01-03 11:00:46 +0200

    _

--- frontend/src/components/contentBlocks/HeaderModed/index.jsx
+++ frontend/src/components/contentBlocks/HeaderModed/index.jsx
@@ -9,8 +9,8 @@ const HeaderModed = () => {
                     <div className='row header__top'>
                         <div className='col-lg-7 col-md-12 header__top__left px-3'>
                             <div className='row'>
-                                <div className='col-lg-6 col-md-12 text-white mt-4 header__top__left__text'>
-                                    <div>Welcome to Akcel, Crowdfunding & Charity Agency</div>
+                                <div className='col-lg-6 col-md-12 text-white mt-4'>
+                                    <h6>Welcome to Akcel, Crowdfunding & Charity Agency</h6>
                                 </div>
                                 <div className='col-lg-6 col-md-12 mt-3'>
                                     <button className='text-white header__top__left__button-moded d-flex align-items-center justify-content-center'>
@@ -22,13 +22,13 @@ const HeaderModed = () => {
                         </div>
                         <div className='col-lg-5 col-md-12 header__top__right'>
                             <div className='row text-white'>
-                                <div className='col-lg-6 col-md-12 mt-4 d-flex header__top__right__phone-email'>
+                                <div className='col-lg-6 col-md-12 mt-4 d-flex justify-content-end'>
                                     <img src="../image/header_moded_phone.svg" />
-                                    <div className='px-3'>394-091-3312</div>
+                                    <h6 className='px-3'>394-091-3312</h6>
                                 </div>
-                                <div className='col-lg-6 col-md-12 mt-4 d-flex header__top__right__phone-email'>
+                                <div className='col-lg-6 col-md-12 mt-4 d-flex justify-content-end'>
                                     <img src="../image/header_moded_email.svg" />
-                                    <div style={{ marginLeft: '15px' }}>support@akcel.com</div>
+                                    <h6 style={{ marginLeft: '15px' }}>support@akcel.com</h6>
                                 </div>
                             </div>
                         </div>
@@ -38,33 +38,35 @@ const HeaderModed = () => {
                 <div className='container mt-4 header-bottom pb-4'>
                     <div className='row d-flex'>
                         <div className='col-lg-3 col-md-12 header-bottom__logo'>
-                            <img src="../image/header_moded_logo.svg" />
+                            <a href="/">
+                                <img src="/image/header_moded_logo.svg" />
+                            </a>
                         </div>
                         <div className='col-lg-6'>
                             <div className='row text-center d-flex justify-content-center align-items-center'>
-                                <div className='col mt-4'>
-                                    <a href='' className='header-bottom__ref-moded'>Home</a>
-                                </div>
-                                <div className='col mt-4'>
-                                    <a href='' className='header-bottom__ref-moded'>About</a>
-                                </div>
+                                <h5 className='col mt-4'>
+                                    <a href='/' className='header-bottom__ref-moded'>Home</a>
+                                </h5>
+                                <h5 className='col mt-4'>
+                                    <a href='/about-us' className='header-bottom__ref-moded'>About</a>
+                                </h5>
                                 <div className='col mt-4'>
                                     <div className='d-flex justify-content-center'>
-                                        <div className='header-bottom__ref-moded mx-4'>Projects</div>
-                                        <img src='./image/header_down_arrow_white.svg' />
+                                        <h5 className='header-bottom__ref-moded mx-4'>Projects</h5>
+                                        <img alt="#" src='/image/header_down_arrow_white.svg' />
                                     </div>
                                 </div>
-                                <div className='col mt-4'>
-                                    <a href='' className='header-bottom__ref-moded'>News</a>
-                                </div>
-                                <div className='col mt-4'>
-                                    <a href='' className='header-bottom__ref-moded'>Contact Us</a>
-                                </div>
+                                <h5 className='col mt-4'>
+                                    <a href='/latest-news' className='header-bottom__ref-moded'>News</a>
+                                </h5>
+                                <h5 className='col mt-4'>
+                                    <a href='/contact-us' className='header-bottom__ref-moded'>Contact Us</a>
+                                </h5>
                             </div>
                         </div>
-                        <div className='col-lg-3 col-md-12 mt-2 d-flex header-bottom__button'>
-                            <button className='header-bottom__button__button-moded'>
-                                <img src="../image/header_moded_user.svg" />
+                        <div className='col-lg-3 col-md-12 mt-2 d-flex justify-content-end'>
+                            <button>
+                                <img alt="user" src="/image/header_moded_user.svg" />
                                 <span className='mx-3 text-white'>My Account</span>
                             </button>
                         </div>
@@ -75,13 +77,13 @@ const HeaderModed = () => {
                 <div className='header_modes-bg'>
                     <div className='container header_modes'>
                         <div style={{ backgroundImage: `url(${background})` }} className='header_modes-bg-picture'>
-                            <div className='text-center text-white header_modes__title'>Let’s find a crowdfunding projects around the world</div>
+                            <h2 className='text-center text-white header_modes__title'>Let’s find a crowdfunding projects around the world</h2>
                         </div>
                     </div>
                 </div>
                 <div className='input pb-2 row'>
                     <div className='col-lg-3 col-sm-12 d-flex align-items-center justify-content-center px-5'>
-                        <div className='input__category'>All Category</div>
+                        <h5 className=''>All Category</h5>
                         <img src='../image/header_down_arrow.svg' style={{ marginLeft: '30px' }} />
                     </div>
                     <div className='col-lg-8 col-sm-12 d-flex align-items-center'>
@@ -93,16 +95,10 @@ const HeaderModed = () => {
                 </div>
             </section>
             <section className='popular_search text-center row'>
-                <div className='col-lg-3 col-md-12 mt-3 popular_search__search' style={{fontWeight: '500'}}>Popular Search:</div>
-                <div className='col-lg-3 col-md-4 mt-3'>
-                    <a href='' className='popular_search__search search color1'>Technology Projects</a>
-                </div>
-                <div className='col-lg-3 col-md-4 mt-3 color1 popular_search__search search'>
-                    <a href='' className='popular_search__search search color1'>Cancer Charity Programs</a>
-                </div>
-                <div className='col-lg-3 col-md-4  mt-3 color1 popular_search__search search'>
-                    <a href='' className='popular_search__search search color1'>Design Interior</a>
-                </div>
+                <h5 className='col-lg-3 col-md-12 mt-3'>Popular Search:</h5>
+                <h5 className='col-lg-3 col-md-4 mt-3 text-success'>Technology Projects</h5>
+                <h5 className='col-lg-3 col-md-4 mt-3 text-success'>Cancer Charity Programs</h5>
+                <h5 className='col-lg-3 col-md-4 mt-3 text-success'>Design Interior</h5>
             </section>
         </section>
     )
