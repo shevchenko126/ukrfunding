fa09a935d2166e701ff9c6ecd2c0bfa3ddaca763
Danylo Diachenko <danildiachenko23@gmail.com>
2023-01-03 11:07:30 +0200

    _

--- frontend/src/components/contentBlocks/HeaderModed/index.jsx
+++ frontend/src/components/contentBlocks/HeaderModed/index.jsx
@@ -13,7 +13,7 @@ const HeaderModed = () => {
                                     <h6>Welcome to Akcel, Crowdfunding & Charity Agency</h6>
                                 </div>
                                 <div className='col-lg-6 col-md-12 mt-3'>
-                                    <button className='text-white header__top__left__button-moded d-flex align-items-center justify-content-center'>
+                                    <button className='text-white sign-up-btn d-flex align-items-center justify-content-center'>
                                         <img src="../image/header_moded_login.svg" />
                                         <div className='mx-2 text-success'>Sign Up Now</div>
                                     </button>
@@ -45,27 +45,27 @@ const HeaderModed = () => {
                         <div className='col-lg-6'>
                             <div className='row text-center d-flex justify-content-center align-items-center'>
                                 <h5 className='col mt-4'>
-                                    <a href='/' className='header-bottom__ref-moded'>Home</a>
+                                    <a href='/' className='header-ref'>Home</a>
                                 </h5>
                                 <h5 className='col mt-4'>
-                                    <a href='/about-us' className='header-bottom__ref-moded'>About</a>
+                                    <a href='/about-us' className='header-ref'>About</a>
                                 </h5>
                                 <div className='col mt-4'>
                                     <div className='d-flex justify-content-center'>
-                                        <h5 className='header-bottom__ref-moded mx-4'>Projects</h5>
+                                        <h5 className='header-ref mx-4'>Projects</h5>
                                         <img alt="#" src='/image/header_down_arrow_white.svg' />
                                     </div>
                                 </div>
                                 <h5 className='col mt-4'>
-                                    <a href='/latest-news' className='header-bottom__ref-moded'>News</a>
+                                    <a href='/latest-news' className='header-ref'>News</a>
                                 </h5>
                                 <h5 className='col mt-4'>
-                                    <a href='/contact-us' className='header-bottom__ref-moded'>Contact Us</a>
+                                    <a href='/contact-us' className='header-ref'>Contact Us</a>
                                 </h5>
                             </div>
                         </div>
                         <div className='col-lg-3 col-md-12 mt-2 d-flex justify-content-end'>
-                            <button>
+                            <button className='button-bg-pink-text-white'>
                                 <img alt="user" src="/image/header_moded_user.svg" />
                                 <span className='mx-3 text-white'>My Account</span>
                             </button>
@@ -74,27 +74,27 @@ const HeaderModed = () => {
                 </div>
             </section>
             <section>
-                <div className='header_modes-bg'>
+                <div className='header-bg'>
                     <div className='container header_modes'>
-                        <div style={{ backgroundImage: `url(${background})` }} className='header_modes-bg-picture'>
-                            <h2 className='text-center text-white header_modes__title'>Let’s find a crowdfunding projects around the world</h2>
+                        <div style={{ backgroundImage: `url(${background})` }} className='header-bg-picture'>
+                            <h2 className='text-center text-white header-title'>Let’s find a crowdfunding projects around the world</h2>
                         </div>
                     </div>
                 </div>
-                <div className='input pb-2 row'>
+                <div className='header-input-container pb-2 row'>
                     <div className='col-lg-3 col-sm-12 d-flex align-items-center justify-content-center px-5'>
                         <h5 className=''>All Category</h5>
                         <img src='../image/header_down_arrow.svg' style={{ marginLeft: '30px' }} />
                     </div>
                     <div className='col-lg-8 col-sm-12 d-flex align-items-center'>
-                        <input className='input__input' placeholder='Find Projects' type='text' min='3' max='50' />
+                        <input className='header-input' placeholder='Find Projects' type='text' min='3' max='50' />
                     </div>
                     <div className='col-lg-1 col-sm-12 d-flex align-items-center justify-content-center'>
                         <img src='../image/header_bottom_search_moded.svg' />
                     </div>
                 </div>
             </section>
-            <section className='popular_search text-center row'>
+            <section className='header-popular-search text-center row'>
                 <h5 className='col-lg-3 col-md-12 mt-3'>Popular Search:</h5>
                 <h5 className='col-lg-3 col-md-4 mt-3 text-success'>Technology Projects</h5>
                 <h5 className='col-lg-3 col-md-4 mt-3 text-success'>Cancer Charity Programs</h5>
