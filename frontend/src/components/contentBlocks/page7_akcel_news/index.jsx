ca7f9bcd46a4029d5fb1dede17b001db06743978
Danylo Diachenko <danildiachenko23@gmail.com>
2023-01-04 12:09:33 +0200

    _

--- frontend/src/components/contentBlocks/page7_akcel_news/index.jsx
+++ frontend/src/components/contentBlocks/page7_akcel_news/index.jsx
@@ -1,10 +1,8 @@
+import Select from 'react-select'
 import './index.css';
 import OneNews from '../Items/OneNews';
 
 const AkcelNews_page7 = (props) => {
-
-   
-
     return (
         <section>
             <article>
@@ -14,29 +12,50 @@ const AkcelNews_page7 = (props) => {
                 </div>
             </article>
             <article>
-                <div className='container filters'>
-                    <div className='row filters__left'>
-                        <div className='col-xl-6 col-lg-12 mt-5 d-flex filters__left-input'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-xl-6 col-lg-12 mt-5 d-flex'>
                             <input type='text' placeholder='Search Articles' min='5' max='50' className='color1 filters__left__input px-4' />
                         </div>
-                        <div className='col-xl-6 col-lg-12 p-0 row filters__right'>
-                            <div className="col-xl-5 col-lg-12 mt-5 d-flex filters__right-col">
-                                <div className='d-flex px-3 align-items-center filters__right__categories'>
-                                    <div className='color1 filters__right__categories__text'>All Categories</div>
-                                    <select className='filters__right__categories__select'>
-                                        <img src='../image/header_down_arrow.svg' />
-                                        {/* <option>hello world</option> */}
-                                    </select>
-                                </div>
+                        <div className='col-xl-6 col-lg-12 p-0 row'>
+                            <div className="col-xl-8 col-lg-12 mt-5 d-flex filters__right-col">
+                                <Select
+                                    options={[
+                                        { value: 'all', label: 'All Categories' },
+                                        { value: 'technology', label: 'Technology' },
+                                        { value: 'education', label: 'Education' },
+                                        { value: 'videos', label: 'Videos' },
+                                        { value: 'medical', label: 'Medical' },
+                                    ]}
+                                    defaultValue={{ value: 'all', label: 'All Categories' }}
+                                    styles={{
+                                        control: (baseStyles, state) => ({
+                                            ...baseStyles,
+                                            border: 'none',
+                                            background: 'rgba(163, 163, 163, 0.15)',   
+                                            width: '200px',   
+                                            paddingLeft: '10px'                                
+                                        }),
+                                    }}
+                                />
                             </div>
-                            <div className="col-xl-3 col-lg-12 mt-5 p-0 d-flex filters__right-col">
-                                <div className='d-flex justify-content-between px-4 align-items-center filters__right__time'>
-                                    <div className='color1'>Latest</div>
-                                    <select className='filters__right__categories__select date'>
-                                        <img src='../image/header_down_arrow.svg' />
-                                        {/* <option>hello world</option> */}
-                                    </select>
-                                </div>
+                            <div className="col-xl-4 col-lg-12 mt-5 p-0 d-flex filters__right-col">
+                                <Select
+                                    options={[
+                                        { value: 'latest', label: 'Latest' },
+                                        { value: 'newest', label: 'Newest' },
+                                    ]}
+                                    defaultValue={{ value: 'latest', label: 'Latest' }}
+                                    styles={{
+                                        control: (baseStyles, state) => ({
+                                            ...baseStyles,
+                                            border: 'none',
+                                            background: 'rgba(163, 163, 163, 0.15)',
+                                            width: '175px',   
+                                            paddingLeft: '10px' 
+                                        }),
+                                    }}
+                                />
                             </div>
                         </div>
                     </div>
