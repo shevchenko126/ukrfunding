aa12ed2a9c8db1a843fa46551f6943139841e4cf
Danylo Diachenko <danildiachenko23@gmail.com>
2023-01-04 15:58:33 +0200

    _

--- frontend/src/components/pages/11_ProjectCategories.jsx
+++ frontend/src/components/pages/11_ProjectCategories.jsx
@@ -5,11 +5,48 @@ import BecomeOurPremPartner from '../contentBlocks/BecomeOurPremiumPartner';
 import Footer from '../Footer';
 
 export const ProjectCategories_11 = () => { // page 11
+
+    const projects = [
+        {
+            title: 'Mirror One | Your life at a glance',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip',
+            author: 'Gustavo Mango',
+            /* rate: '' */
+            campaign: '12 Campaign',
+            place: 'New York, London',
+            progress: '4',
+            raised: 'Total Raised $ 5,345',
+            days_left: '45 Days left',
+        },
+        {
+            title: 'Mirror One | Your life at a glance',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip',
+            author: 'Roger Bergson',
+            /* rate: '' */
+            campaign: '12 Campaign',
+            place: 'New York, London',
+            progress: '94',
+            raised: 'Total Raised $ 5,345',
+            days_left: '45 Days left',
+        },
+        {
+            title: 'Mirror One | Your life at a glance',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip',
+            author: 'James Westervelt',
+            /* rate: '' */
+            campaign: '12 Campaign',
+            place: 'New York, London',
+            progress: '32',
+            raised: 'Total Raised $ 5,345',
+            days_left: '45 Days left',
+        },
+    ];
+
     return (
         <section>
             <Header />
             <Categories />
-            <Page115Projects />
+            <Page115Projects projects={projects} />
             <BecomeOurPremPartner />
             <Footer />
         </section>
