bd6afc93de0e7979f9d913dbf0330e067bf78cd1
Danylo Diachenko <danildiachenko23@gmail.com>
2023-01-04 14:41:15 +0200

    _

--- frontend/src/components/contentBlocks/page8NewsDetails/index.jsx
+++ frontend/src/components/contentBlocks/page8NewsDetails/index.jsx
@@ -58,7 +58,7 @@ const NewsDetails = () => {
 				<h4 className="mt-5 pt-5">The standard Lorem Ipsum passage, used since the 1500s</h4>
 				<p className="mt-3">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 				<div className="row">
-					<div className="col-lg-4 col-sm-12 news_details__share-article">Share this article :</div>
+					<div className="col-lg-4 col-sm-12 share-title">Share this article :</div>
 					<div className="col-lg-8 col-sm-12 mt-5 row">
 						<div className='col-lg-3 col-sm-12 btn-share-col'>
 							<button className="btn-share">
