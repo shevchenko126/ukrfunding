7ef6428d8e6309f420a275cd374cc34c2e459d37
Danylo Diachenko <danildiachenko23@gmail.com>
2023-01-04 14:59:34 +0200

    _

--- frontend/src/components/pages/08_NewsDetails.jsx
+++ frontend/src/components/pages/08_NewsDetails.jsx
@@ -1,16 +1,47 @@
 import Header from '../Header';
 import NewsDetails from '../contentBlocks/page8NewsDetails';
 import WriteComment from '../contentBlocks/page8WriteComment';
-import Page8Related from '../contentBlocks/page8News';
+import Page8RelatedNews from '../contentBlocks/page8News/index.jsx';
 import Footer from '../Footer';
 
 export const NewsDetails_08 = () => { // page 8
+
+    const news = [
+        {
+            category: 'health',
+            title: 'New vaccine for cattle protects against fetal calf loss from EBA – anatomy of..',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            author: 'Hawkins Junior',
+            timeRead: '24min read',
+            date: 'November 21th, 2020',
+            slug: 'dgfvg321gf',
+        },
+        {
+            category: 'business',
+            title: '4 Things parents learned for they jids in 2020',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            author: 'Hawkins Junior',
+            timeRead: '24min read',
+            date: 'November 21th, 2020',
+            slug: 'dgfvg321gf',
+        },
+        {
+            category: 'design',
+            title: 'He Created the Web. Now He’s Out to Remake the Digital World.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            author: 'Melinda Humbles',
+            timeRead: '24min read',
+            date: 'November 21th, 2020',
+            slug: 'dgfvg321gf',
+        },
+    ];
+
     return (
         <section>
             <Header />
             <NewsDetails />
             <WriteComment />
-            <Page8Related />
+            <Page8RelatedNews news={news} />
             <div style={{ marginTop: '100px' }}>
                 <Footer />
             </div>
