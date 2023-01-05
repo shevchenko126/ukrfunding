50b41eebd75f5c29ba1ce7720562aa4fc28776f7
Danylo Diachenko <danildiachenko23@gmail.com>
2023-01-05 11:05:40 +0200

    _

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,19 @@ import './App.css';
 /* Pages */
 import { HomePage_01 } from './components/pages/01_HomePage';
 import { AboutUs_02 } from './components/pages/02_AboutUs';
-import { Project4Col_03 } from './components/pages/03_Project4Col';
-import { Project3Col_04 } from './components/pages/04_Project3Col';
-import { ProjectDetails_05 } from './components/pages/05_ProjectDetails';
-import { ContactUs_06 } from './components/pages/06_ContactUs';
-import { LatestNews_07 } from './components/pages/07_LatestNews';
-import { NewsDetails_08 } from './components/pages/08_NewsDetails';
-import { TeamMember_09 } from './components/pages/09_TeamMember';
-import { SuccessProjectStory_10 } from './components/pages/10_SuccessProject';
-import { ProjectCategories_11 } from './components/pages/11_ProjectCategories';
-import { ChangePassword } from './components/pages/12_ChangePassword';
-import { Register } from './components/pages/13_Register';
-import { Login } from './components/pages/14_Login';
-import { AddProject } from './components/pages/15_AddProject';
-import { PageNotFound } from './components/pages/16_PageNotFound';
+import { Project3Col_04 } from './components/pages/03_Project3Col';
+import { ProjectDetails_05 } from './components/pages/04_ProjectDetails';
+import { ContactUs_06 } from './components/pages/05_ContactUs';
+import { LatestNews_07 } from './components/pages/06_LatestNews';
+import { NewsDetails_08 } from './components/pages/07_NewsDetails';
+import { TeamMember_09 } from './components/pages/08_TeamMember';
+import { SuccessProjectStory_10 } from './components/pages/9_SuccessProject';
+import { ProjectCategories_11 } from './components/pages/10_ProjectCategories';
+import { ChangePassword } from './components/pages/11_ChangePassword';
+import { Register } from './components/pages/12_Register';
+import { Login } from './components/pages/13_Login';
+import { AddProject } from './components/pages/14_AddProject';
+import { PageNotFound } from './components/pages/15_PageNotFound';
 
 function App() {
   return (
@@ -27,7 +26,6 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage_01 />} />
           <Route path="/about" element={<AboutUs_02 />} />
-          {/* <Route path="/projects-4-col" element={<Project4Col_03 />} /> */}
           <Route path="/projects" element={<Project3Col_04 />} />
           <Route path="/project/:project" element={<ProjectDetails_05 />} />
           <Route path="/contact-us" element={<ContactUs_06 />} />
