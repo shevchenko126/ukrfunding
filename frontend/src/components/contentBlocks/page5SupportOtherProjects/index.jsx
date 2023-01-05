5cbda5234f7dba732c4a513a8436cdac4aae726b
Danylo Diachenko <danildiachenko23@gmail.com>
2023-01-05 13:49:27 +0200

    Update index.jsx

--- frontend/src/components/contentBlocks/page5SupportOtherProjects/index.jsx
+++ frontend/src/components/contentBlocks/page5SupportOtherProjects/index.jsx
@@ -9,23 +9,14 @@ const Support = () => {
 				<a href='' className='project-support'>Gen3 Airoha Air Pro 3 TWS Cancel Headphone</a>
 			</h4>
 			<p style={{ maxWidth: '404px' }} className='mt-4 mx-auto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
-			<div className='row' style={{ maxWidth: '404px', margin: 'auto' }}>
-				<div className="row">
-					<div className="col-xl-2 col-lg-12 p-0 mt-4">
+			<div style={{ maxWidth: '404px', margin: 'auto' }}>
+				<div className="d-flex p-0 mt-3">
+					<div>
 						<img src="/image/page5_support_profile.svg" />
 					</div>
-					<div className="col-xl-9 col-lg-12 p-0 mt-4 author-right">
-						<div className="d-flex">
-							<div className="author-name">Gustavo Mango</div>
-							<img src="/image/page5_support_star.svg" style={{ marginLeft: '10px' }} />
-							<img src="/image/page5_support_star.svg" className="px-1" />
-							<img src="/image/page5_support_star.svg" />
-						</div>
-						<div class="d-flex mt-2">
-							<div className="text-success author-campaign">12 Campaign</div>
-							<img src="/image/page5_support_dot.svg" className="mx-3" />
-							<div className="author-city">New York, London</div>
-						</div>
+					<div className='mx-3'>
+						<div className="author-name">Gustavo Mango</div>
+						<div className="author-city">New York, London</div>
 					</div>
 				</div>
 			</div>
@@ -38,23 +29,14 @@ const Support = () => {
 				<a href='' className='project-support'>Gen3 Airoha Air Pro 3 TWS Cancel Headphone</a>
 			</h4>
 			<p style={{ maxWidth: '404px' }} className='mt-4 mx-auto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
-			<div className='row' style={{ maxWidth: '404px', margin: 'auto' }}>
-				<div className="row">
-					<div className="col-xl-2 col-lg-12 p-0 mt-4">
+			<div style={{ maxWidth: '404px', margin: 'auto' }}>
+				<div className="d-flex p-0 mt-3">
+					<div>
 						<img src="/image/page5_support_profile.svg" />
 					</div>
-					<div className="col-xl-9 col-lg-12 p-0 mt-4 author-right">
-						<div className="d-flex">
-							<div className="author-name">Gustavo Mango</div>
-							<img src="/image/page5_support_star.svg" style={{ marginLeft: '10px' }} />
-							<img src="/image/page5_support_star.svg" className="px-1" />
-							<img src="/image/page5_support_star.svg" />
-						</div>
-						<div class="d-flex mt-2">
-							<div className="text-success author-campaign">12 Campaign</div>
-							<img src="/image/page5_support_dot.svg" className="mx-3" />
-							<div className="author-city">New York, London</div>
-						</div>
+					<div className='mx-3'>
+						<div className="author-name">Gustavo Mango</div>
+						<div className="author-city">New York, London</div>
 					</div>
 				</div>
 			</div>
