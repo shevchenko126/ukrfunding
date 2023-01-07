ff9c1e9d40054e6626de6502c9a31b6f1e4928a2
Danylo Diachenko <danildiachenko23@gmail.com>
2023-01-07 21:47:28 +0200

    _

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -12,12 +12,14 @@ const Header = () => {
             <div className='container'>
                 <div className='row d-flex align-items-center mt-4'>
                     <div className='col-lg-6 col-md-12 px-3 title row d-flex align-items-center'>
-                        <h6 className='col-lg-12 col-md-12 title'>Welcome to Akcel, Crowdfunding & Charity Agency</h6>
+                        <h6 className='col-lg-12 col-md-12 title-title'>Welcome to Akcel, Crowdfunding & Charity Agency</h6>
                         <div className='col-md-12 btn-top title'>
-                            <button className='button-bg-white-text-pink'>
-                                <img alt="user" src='/image/header_bottom_user.svg' />
-                                <span className='mx-3'>{user === 'loginned' ? 'My Account' : user === 'registered' ? 'Sign In Now' : 'Sign Up Now'}</span>
-                            </button>
+                            <Link to={user === 'loginned' ? '/profile' : user === 'registered' ? '/login' : '/register'}>
+                                <button className='button-bg-white-text-pink'>
+                                    <img alt="user" src='/image/header_bottom_user.svg' />
+                                    <span className='mx-3'>{user === 'loginned' ? 'My Account' : user === 'registered' ? 'Sign In Now' : 'Sign Up Now'}</span>
+                                </button>
+                            </Link>
                         </div>
                     </div>
                     <div className='col-lg-6 col-md-12 contacts'>
@@ -37,34 +39,38 @@ const Header = () => {
             <hr style={{ width: '100%', color: 'rgba(0, 0, 0, 0.5)' }} className='mt-4' />
             <div className='container mt-4 pb-4'>
                 <div className='row d-flex align-items-center'>
-                    <a href="/" className='col-lg-3 col-md-6'>
-                        <img src="/image/logo_Akcel.png" alt="logo" />
-                    </a>
+                    <div className='col-lg-3 col-6'>
+                        <a href="/">
+                            <img className='logo-icon' src="/image/logo_Akcel.png" alt="logo" />
+                        </a>
+                    </div>
                     <div className='col-lg-6 header-refs'>
                         <div className='row text-center d-flex justify-content-center align-items-center'>
-                            <h5 className='col mt-4'>
+                            <h5 className='col'>
                                 <Link to='/' className='header-bottom__ref'>Home</Link>
                             </h5>
-                            <h5 className='col mt-4'>
+                            <h5 className='col'>
                                 <Link to='/about' className='header-bottom__ref'>About</Link>
                             </h5>
-                            <h5 className='col mt-4'>
+                            <h5 className='col'>
                                 <Link to='/projects' className='header-bottom__ref'>Projects</Link>
                             </h5>
-                            <h5 className='col mt-4'>
+                            <h5 className='col'>
                                 <Link to='/news' className='header-bottom__ref'>News</Link>
                             </h5>
-                            <h5 className='col mt-4'>
+                            <h5 className='col'>
                                 <Link to='/contact' className='header-bottom__ref'>Contact Us</Link>
                             </h5>
                         </div>
                     </div>
-                    <div className='col-lg-3 col-md-6 mt-2'>
+                    <div className='col-lg-3 col-6 mt-2'>
                         <div className='header-bottom__button'>
-                            <button className='button-bg-white-text-pink'>
-                                <img alt="user" src='/image/header_bottom_user.svg' />
-                                <span className='mx-3'>{user === 'loginned' ? 'My Account' : user === 'registered' ? 'Sign In Now' : 'Sign Up Now'}</span>
-                            </button>
+                            <Link to={user === 'loginned' ? '/profile' : user === 'registered' ? '/login' : '/register'}>
+                                <button className='button-bg-white-text-pink'>
+                                    <img alt="user" src='/image/header_bottom_user.svg' />
+                                    <span className='mx-3'>{user === 'loginned' ? 'My Account' : user === 'registered' ? 'Sign In Now' : 'Sign Up Now'}</span>
+                                </button>
+                            </Link>
                         </div>
                         <div className='burger-menu-col'>
                             <AiOutlineMenu className='burger-menu' />
